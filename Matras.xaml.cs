6be714c7dfbd0fd9aa089b0b87771fc14131dfce
P0roSad <115632790+P0roSad@users.noreply.github.com>
2022-11-18 13:39:40 +0300

    Add files via upload

---
+++ Matras.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sport
+{
+    /// <summary>
+    /// Логика взаимодействия для Matras.xaml
+    /// </summary>
+    public partial class Matras : Window
+    {
+        public Matras()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
